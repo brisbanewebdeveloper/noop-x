a2e55c2ed6491a45e306271dcbd2f8e496cc9c36
Graham Fairweather <xotic750@gmail.com>
2019-08-28 16:36:42 +0200

    Maintenance

--- dist/noop-x.js
+++ dist/noop-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2019-present",
-  "date": "2019-08-27T19:17:13.304Z",
+  "date": "2019-08-28T14:36:37.493Z",
   "describe": "",
   "description": "This method returns undefined.",
   "file": "noop-x.js",
